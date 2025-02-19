b8104d00d93fcb92b9de5439cff02326e571337b
Михаил Жирок <mihail.zhirok@gmail.com>
2025-02-19 22:31:14 +0200

    pagin-commit

--- src/components/AllOrders.tsx
+++ src/components/AllOrders.tsx
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OrderItem from './OrderItem';
 import { OrderItemInt } from '../types/typeIndex';
 
 interface AllOrdersProps {
   orders: OrderItemInt[];
-  openCart:(value: boolean)=>void;
+  openCart: (value: boolean) => void;
 }
 
+const ITEMS_PER_PAGE = 6; 
+
 const AllOrders: React.FC<AllOrdersProps> = ({ openCart, orders }) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const totalPages = Math.ceil(orders.length / ITEMS_PER_PAGE);
+
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const paginatedOrders = orders.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+
   return (
     <>
-      {orders.length ? (
-        orders.map((item) => (
-          <OrderItem openCart={openCart} key={item.id} orderItem={item} />  
+    <div className='allOrdersWrap'>
+
+      {paginatedOrders.length ? (
+        paginatedOrders.map((item) => (
+          <OrderItem openCart={openCart} key={item.id} orderItem={item} />
         ))
       ) : (
         <p>No orders available</p>
       )}
+
+
+     
+    </div>
+    {totalPages > 1 && (
+  <div className="pagination">
+    <button
+      disabled={currentPage === 1}
+      onClick={() => {
+        setCurrentPage((prev) => prev - 1);
+        window.scrollTo({ top: 0, behavior: 'instant'});
+      }}
+    >
+      Prev
+    </button>
+
+    {Array.from({ length: totalPages }, (_, i) => (
+      <button
+        key={i + 1}
+        className={`paginationButton ${currentPage === i + 1 ? 'active' : ''}`}
+        onClick={() => {
+          setCurrentPage(i + 1);
+          window.scrollTo({ top: 0, behavior: 'instant'});
+        }}
+      >
+        {i + 1}
+      </button>
+    ))}
+
+    <button
+      disabled={currentPage === totalPages}
+      onClick={() => {
+        setCurrentPage((prev) => prev + 1);
+        window.scrollTo({ top: 0, behavior: 'instant' });
+      }}
+    >
+      Next
+    </button>
+  </div>
+)}
+
     </>
   );
 };
